b9f6162adfdfc13e3609d32146afab8c6ed5f0fd
Quy Pham <quy.pham@enouvo.com>
2025-07-08 10:56:45 +0700

    chore: status class detail

--- src/features/classDetail/constant/statusEnum.js
+++ src/features/classDetail/constant/statusEnum.js
@@ -1,5 +1,5 @@
 export const statusOptions = {
   NOT_STARTED: { label: "Not started", bg: "#E5E7EB", text: "#374151" },
   ON_GOING: { label: "Ongoing", bg: "#E1E8FF", text: "#1C3FB7" },
-  COMPLETE: { label: "Complete", bg: "#DAF8E6", text: "#1A8245" },
+  COMPLETED: { label: "Completed", bg: "#DAF8E6", text: "#1A8245" },
 };
