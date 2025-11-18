9ed92a63d52cf381cc91f9b17d19e89289689ad7
Quy Pham <quy.pham@stunited.vn>
2025-11-18 09:57:45 +0700

    chore: handle sidebar

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -1,63 +1,63 @@
-import { lazy } from "react";
-import { ProtectedRoute } from "./ProtectedRoute/ProtectedRoute.jsx";
-import GradingPage from "@pages/grading/GradingPage.jsx";
-import SessionLayout from "../../pages/SessionManagement/SessionLayout.jsx";
-import SessionInformation from "@pages/SessionManagement/SessionInformation.jsx";
-import { TableType } from "@features/session/constant/TableEnum.js";
-const ProfilePage = lazy(() => import("@pages/Profile/index.jsx"));
-import Dashboard from "@pages/Dashboard/Dashboard.jsx";
-import ClassDetail from "@pages/ClassDetail/ClassDetail.jsx";
-import TeacherAccountManagement from "@pages/TeacherManagement/TeacherAccountManagement.jsx";
-import ClassManagement from "@pages/ClassManagement/index.jsx";
-import RedirectByRole from "./RedirectByRole/index.jsx";
+import { lazy } from 'react';
+import { ProtectedRoute } from './ProtectedRoute/ProtectedRoute.jsx';
+import GradingPage from '@pages/grading/GradingPage.jsx';
+import SessionLayout from '../../pages/SessionManagement/SessionLayout.jsx';
+import SessionInformation from '@pages/SessionManagement/SessionInformation.jsx';
+import { TableType } from '@features/session/constant/TableEnum.js';
+const ProfilePage = lazy(() => import('@pages/Profile/index.jsx'));
+import Dashboard from '@pages/Dashboard/Dashboard.jsx';
+import ClassDetail from '@pages/ClassDetail/ClassDetail.jsx';
+import TeacherAccountManagement from '@pages/TeacherManagement/TeacherAccountManagement.jsx';
+import ClassManagement from '@pages/ClassManagement/index.jsx';
+import RedirectByRole from './RedirectByRole/index.jsx';
 
 const PrivateRoute = [
   {
-    path: "/",
+    path: '/',
     element: <ProtectedRoute />,
-    breadcrumb: "Home",
+    breadcrumb: 'Home',
     children: [
       {
         index: true,
         element: <RedirectByRole />,
-        breadcrumb: "Dashboard",
+        breadcrumb: 'Dashboard',
       },
       {
-        path: "dashboard",
+        path: 'dashboard',
         element: <Dashboard />,
-        breadcrumb: "Dashboard",
-        role: ["admin"],
+        breadcrumb: 'Dashboard',
+        role: ['admin'],
       },
       {
-        path: "teacher",
-        role: ["admin"],
-        breadcrumb: "Teacher",
+        path: 'teacher',
+        role: ['admin'],
+        breadcrumb: 'Teacher',
         element: <TeacherAccountManagement />,
       },
       {
-        path: "class",
-        role: ["teacher"],
-        breadcrumb: "Class Management",
+        path: 'class',
+        role: ['teacher'],
+        breadcrumb: 'Class Management',
         children: [
           {
             index: true,
             element: <ClassManagement />,
           },
           {
-            path: ":classId",
-            breadcrumb: "Class Detail",
+            path: ':classId',
+            breadcrumb: 'Class Detail',
             children: [
               {
                 index: true,
                 element: <ClassDetail />,
               },
               {
-                path: "session",
+                path: 'session',
                 element: <SessionLayout />,
                 children: [
                   {
-                    path: ":sessionId",
-                    breadcrumb: "Session Detail",
+                    path: ':sessionId',
+                    breadcrumb: 'Session Detail',
                     children: [
                       {
                         index: true,
@@ -66,11 +66,11 @@ const PrivateRoute = [
                         ),
                       },
                       {
-                        path: "student",
+                        path: 'student',
                         children: [
                           {
-                            path: ":studentId",
-                            breadcrumb: "Student Detail",
+                            path: ':studentId',
+                            breadcrumb: 'Student Detail',
                             children: [
                               {
                                 index: true,
@@ -85,11 +85,11 @@ const PrivateRoute = [
                         ],
                       },
                       {
-                        path: "participant",
+                        path: 'participant',
                         children: [
                           {
-                            path: ":participantId",
-                            breadcrumb: "Participant Detail",
+                            path: ':participantId',
+                            breadcrumb: 'Participant Detail',
                             children: [
                               {
                                 index: true,
@@ -108,13 +108,13 @@ const PrivateRoute = [
         ],
       },
       {
-        path: "profile",
-        breadcrumb: "Profile",
+        path: 'profile',
+        breadcrumb: 'Profile',
         children: [
           {
-            path: "",
+            path: '',
             element: <ProfilePage />,
-            breadcrumb: "",
+            breadcrumb: '',
           },
         ],
       },
