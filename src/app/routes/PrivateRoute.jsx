3502c1400de2510eab89bc6faa0cc44354cf83f8
T-meoz <144752332+T-meoz@users.noreply.github.com>
2025-11-18 11:42:48 +0700

    feat: [GPS-9] Implement to Edit the existing questions

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -1,15 +1,17 @@
-import { lazy } from 'react';
-import { ProtectedRoute } from './ProtectedRoute/ProtectedRoute.jsx';
-import GradingPage from '@pages/grading/GradingPage.jsx';
-import SessionLayout from '../../pages/SessionManagement/SessionLayout.jsx';
-import SessionInformation from '@pages/SessionManagement/SessionInformation.jsx';
-import { TableType } from '@features/session/constant/TableEnum.js';
-const ProfilePage = lazy(() => import('@pages/Profile/index.jsx'));
-import Dashboard from '@pages/Dashboard/Dashboard.jsx';
-import ClassDetail from '@pages/ClassDetail/ClassDetail.jsx';
-import TeacherAccountManagement from '@pages/TeacherManagement/TeacherAccountManagement.jsx';
-import ClassManagement from '@pages/ClassManagement/index.jsx';
-import RedirectByRole from './RedirectByRole/index.jsx';
+import { lazy } from "react";
+import { ProtectedRoute } from "./ProtectedRoute/ProtectedRoute.jsx";
+import GradingPage from "@pages/grading/GradingPage.jsx";
+import SessionLayout from "../../pages/SessionManagement/SessionLayout.jsx";
+import SessionInformation from "@pages/SessionManagement/SessionInformation.jsx";
+import { TableType } from "@features/session/constant/TableEnum.js";
+const ProfilePage = lazy(() => import("@pages/Profile/index.jsx"));
+import Dashboard from "@pages/Dashboard/Dashboard.jsx";
+import ClassDetail from "@pages/ClassDetail/ClassDetail.jsx";
+import TeacherAccountManagement from "@pages/TeacherManagement/TeacherAccountManagement.jsx";
+import ClassManagement from "@pages/ClassManagement/index.jsx";
+import RedirectByRole from "./RedirectByRole/index.jsx";
+const QuestionBank = lazy(() => import("@pages/QuestionBank/index.jsx"));
+const QuestionDetail = lazy(() => import("@pages/QuestionBank/QuestionDetail"));
 
 const PrivateRoute = [
   {
@@ -35,9 +37,21 @@ const PrivateRoute = [
         element: <TeacherAccountManagement />,
       },
       {
-        path: 'class',
-        role: ['teacher'],
-        breadcrumb: 'Class Management',
+        path: "question-bank",
+        element: <QuestionBank />,
+        breadcrumb: "Question Bank",
+        role: ["teacher", "admin"], // Cấp quyền cho role phù hợp
+      },
+      {
+        path: "question-bank/:id",
+        element: <QuestionDetail />,
+        breadcrumb: "Question Detail",
+        role: ["admin", "teacher"],
+      },
+      {
+        path: "class",
+        role: ["teacher"],
+        breadcrumb: "Class Management",
         children: [
           {
             index: true,
@@ -118,6 +132,22 @@ const PrivateRoute = [
           },
         ],
       },
+      {
+        path: 'questions',
+        breadcrumb: 'Question Bank',
+        children: [
+        {
+          index: true,
+          element: <QuestionBank />,
+        },
+        {
+          path: 'create/:skill',
+          element: <>CreateQuestion</>,
+          breadcrumb: 'Create',  
+          role: ['teacher', 'admin', 'superadmin'],
+        }
+        ]
+      }
     ],
   },
 ];
